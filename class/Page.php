7c5feaf20ab54ef48ec9fecc1ae7ae515456aa96
EC2 Default User <ec2-user@ip-172-31-84-223.ec2.internal>
2019-01-14 02:03:08 +0000

    finished IOC. still needs logging

---
+++ class/Page.php
@@ -0,0 +1,104 @@
+<?php 
+class Page {
+    public $f3;
+    
+    public function __construct($f3)
+    {
+        $web=\Web::instance();
+        
+        $result = $web->request('http://169.254.169.254/latest/meta-data/public-hostname');
+        $f3->set('aws_ec2_hostname', $result[body]);
+        
+        $result = $web->request('http://169.254.169.254/latest/meta-data/instance-id');
+        $f3->set('aws_ec2_instance_id', $result[body]);
+        
+        $f3->set('current_time', date("g:i A l, F j Y."));
+        
+        $client = new Aws\Rds\RdsClient([
+            'version' => 'latest',
+            'region' => 'us-east-1',
+            'profile' => 'default',
+        ]);
+        $result = $client->describeDBInstances();
+        $hostname = $result['DBInstances'][0]['Endpoint']['Address'];
+        $port = $result['DBInstances'][0]['Endpoint']['Port'];
+        $username = $result['DBInstances'][0]['MasterUsername'];
+        $dbconnection = mysqli_connect($hostname, $username, 'password');
+        
+        mysqli_close($dbconnection);
+        $f3->set('aws_rds_dbinstance_endpoint', $hostname);
+        $f3->set('aws_rds_dbinstance_id', $result['DBInstances'][0]['DBInstanceIdentifier']);
+        //$f3->set('DB', new DB\SQL("mysql:host={$hostname};port={$port};dbname={$database}",$username, $password));
+        $this->f3 = $f3;
+    }  
+    
+    function home($f3) {
+        
+        print \Template::instance()->render('base.html');
+    }
+    
+    function login() {
+            // Create a new (and much lighter) OAuth2 client with no external dependencies!!
+        $f3 = $this->f3;
+        $oauth2 = new \Web\OAuth2();
+        $auth_domain = 'https://stackoverflow.com';
+        $redirect_uri = "{$f3->get('SCHEME')}://{$f3->get('HOST')}{$f3->get('BASE')}/login";
+        //$f3->set('debug', k($f3,KRUMO_RETURN));
+        //k($f3);
+        $oauth2->set('redirect_uri', $redirect_uri);
+        $oauth2->set('client_id', '12256');
+        //Step 1: OAuth 2.0 flow (Send a user to /oauth endpoint, with these query string parameters)
+        if ($f3->get('VERB') == 'GET' && !isset($_GET['code'])) {
+
+            // Set the endpoint's required params
+            //                 //$oauth2->set('state', $this->getRandomState());
+            $uri = $oauth2->uri($auth_domain . '/oauth');
+            
+            // Step 2: OAuth 2.0 flow (The user approves your app)
+            header('Location: ' . $uri);
+        }
+        // Step 3: OAuth 2.0 flow (The user is redirected to redirect_uri, with these query string parameters)
+        elseif ($f3->get('VERB') == 'GET' && isset($_GET['code'])) {
+                // Set the endpoint's required params
+            
+            
+                
+                //$oauth2->set('state', $_GET['state']);
+                $oauth2->set('client_secret', '6J4fm2Ok73o20Q5dXO8ZhA((');
+                $oauth2->set('code', $_GET['code']);
+                
+                // Step 4: OAuth 2.0 flow (POST parameters to /oauth/access_token endpoint)
+               $uri = $oauth2->uri($auth_domain . '/oauth/access_token/json');
+                if ($request = $oauth2->request($uri, 'POST'))
+                {
+                    $oauth2 = new \Web\OAuth2();
+                    $web=\Web::instance();
+
+                    $oauth2->set('access_token', $request['access_token']);
+                    $oauth2->set('key', 'jYhy)W5d6ThTUK)iA60swg((');
+                    $oauth2->set('site', 'stackoverflow');
+                    $uri = $oauth2->uri('https://api.stackexchange.com/me',true,false);
+                    //$uri = "https://api.stackexchange.com/2.2/me?access_token={$request['access_token']}&key=jYhy)W5d6ThTUK)iA60swg((&site=stackoverflow";
+                    $options=[
+                        'content'=>$url,
+                        'header'=>['Accept: application/json']
+                    ];
+                    $me = $web->request($uri, $options);
+                    if ($me)
+                        $f3->set('is_authenticated',true);
+                        //k(json_decode($me['body']));
+                    //$f3->set('token', $request);
+                    
+                }
+                //$f3->set('debug', k($request, KRUMO_RETURN));
+                
+                    //$this->f3->set('token', $oauth2->jwt($request['access_token']));
+                    //F3::set('COOKIE.f3token', $request['access_token']);
+                 
+                
+        }
+        print \Template::instance()->render('base.html');
+        
+    }
+}
+    
--- index.php
+++ index.php
@@ -1,143 +1,35 @@
-<html>
-  <head>
-    <title>MetronomeUSA Demo</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
-	<link rel="shortcut icon" href="https://wearemetronome.com/images/phocafavicon/email%20logo.jpg" type="image/x-icon">
-	
-    <style>
-video {
-  object-fit: cover;
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-  top: 0;
-  left: 0;
-}
+<?php
+//Show php debug errors
+ini_set('display_errors', 1);
 
-html, body {
-  height: 100%;
-}
+//Require composer libraries
+require 'vendor/autoload.php';
 
-html {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  font-size: 150%;
-  line-height: 1.4;
-}
+//Initialize F3
+$f3 = \Base::instance();
 
-body {
-  margin: 0;
-}
-
-.viewport-header {
-  position: relative;
-  height: 50vh;
-  text-align: center;
-  /* display: flex; */
-  align-items: center;
-  justify-content: center;
-}
-
-h1 {
-  font-family: 'Syncopate', sans-serif;
-  color: #fff;
-  text-transform: uppercase;
-  letter-spacing: 3vw;
-  line-height: 1.2;
-  font-size: 3vw;
-  text-align: center;
-}
-h1 span {
-  display: block;
-  font-size: 10vw;
-  letter-spacing: 0vw;
-}
-
-main {
-  width: 80vw;
-  left: 10%;
-  height: 40vh;
-  overflow: auto;
-  background: rgba(0, 0, 0, 0.66);
-  color: white;
-  position: relative;
-  padding: 1rem;
-}
-
-.logosize {
-    width: 59px;
-    height: auto;
-}
-
-</style>
-
+//Load F3 Config
+$f3->config('config.ini');
+//$f3->set('debug', k($f3,KRUMO_RETURN));
+//Check to see if there is a database name already set in the F3 config or use the (cleaned) SERVER_NAME
+// if (!($f3->get('DB')))
+// {
+//     $site = new \Site($schema_name);
+//     if ($site->databaseReady() == true)
+//     {
+//         $driver = $site->get_db_driver();
+//         $host = $site->get_db_hostname();
+//         $port = $site->get_db_port();
+//         $username = $site->get_db_username();
+//         $password = $site->get_db_password();
+//         $db = new \DB\SQL("{$driver}:host={$host};port={$port};dbname={$schema_name}", $username, $password);
+//         $f3->set('DB', $db);
+//         //$site->set_db($db);
+//     }
     
-  </head>
-  <body>
-  <!-- <video src="//media.istockphoto.com/videos/fly-over-city-lights-from-space-till-morning-video-id473264399" autoplay loop playsinline muted></video> -->
-  <video src="//ak0.picdn.net/shutterstock/videos/20886700/preview/stock-footage-follow-shot-of-squad-of-soldiers-running-forward-during-military-operation-in-the-desert-slow.mp4" autoplay loop playsinline muted></video>
-
-
-  <header class="viewport-header">
-
-<h1>
-  <a href="https://wearemetronome.com" target="_self" title="Metronome LLC" rel="home">
-                        <img src="https://wearemetronome.com/images/Logo_notext6.png" alt="Metronome LLC" class="logosize">
-            </a>
-    Welcome to
-    <span>Metronome</span>
-  </h1>
-  <div style="display:block"> 
-  <a href="authorize" class="btn btn-lg btn-outline-light" >Login</a>
-  </div>
-</header>
-
-<main>
-    <!--  <h1>Welcome to the AWS CloudFormation PHP Sample</h1> -->
-    <p/>
-    <?php
-      // Print out the current data and tie
-      print "The Current Date and Time is: <br/>";
-      print date("g:i A l, F j Y.");
-    ?>
-    <p/>
-    <?php
-      // Setup a handle for CURL
-      $curl_handle=curl_init();
-      curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
-      curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
-      // Get the hostname of the intance from the instance metadata
-      curl_setopt($curl_handle,CURLOPT_URL,'http://169.254.169.254/latest/meta-data/public-hostname');
-      $hostname = curl_exec($curl_handle);
-      if (empty($hostname))
-      {
-        print "Sorry, for some reason, we got no hostname back <br />";
-      }
-      else
-      {
-        print "Server = " . $hostname . "<br />";
-      }
-      // Get the instance-id of the intance from the instance metadata
-      curl_setopt($curl_handle,CURLOPT_URL,'http://169.254.169.254/latest/meta-data/instance-id');
-      $instanceid = curl_exec($curl_handle);
-      if (empty($instanceid))
-      {
-        print "Sorry, for some reason, we got no instance id back <br />";
-      }
-      else
-      {
-        print "EC2 instance-id = " . $instanceid . "<br />";
-      }
-      $Database   = "mm1n1pmg0j0p576.cgy3hxrvpwxg.us-east-1.rds.amazonaws.com";
-      $DBUser     = "root";
-      $DBPassword = "password";
-      print "Database = " . $Database . "<br />";
-      $dbconnection = mysql_connect($Database, $DBUser, $DBPassword)
-                      or die("Could not connect: " . mysql_error());
-      print ("Connected to $Database successfully");
-      mysql_close($dbconnection);
-    ?>
-    <script src="//gist.github.com/bucs-fan813/927501523300860a1103333a751a8c1a.js"></script>
-</main>
-  </body>
-</html>
+//     if ($schema = $f3->get('schema')) {
+//         $site->schemaReady($schema);
+//     }
+    
+// }
+$f3->run();
