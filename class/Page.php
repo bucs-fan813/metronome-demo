91497e52a4b0bd087f9288bce37c7ad2913b8b63
EC2 Default User <ec2-user@ip-172-31-84-223.ec2.internal>
2019-01-15 19:24:29 +0000

    code refactor and UI enhancements... v0.1-beta

--- class/Page.php
+++ class/Page.php
@@ -1,126 +1,140 @@
 <?php 
 class Page {
+	//Declare globals
     public $f3;
+    private $aws_credentials;
     
     public function __construct($f3)
     {
-        $web=\Web::instance();
-        
-        $result = $web->request('http://169.254.169.254/latest/meta-data/public-hostname');
-        $f3->set('aws_ec2_hostname', $result[body]);
-        
-        $result = $web->request('http://169.254.169.254/latest/meta-data/instance-id');
-        $f3->set('aws_ec2_instance_id', $result[body]);
-        
-        $f3->set('current_time', date("g:i A l, F j Y."));
-        
-        $client = new Aws\Rds\RdsClient([
+		$this->aws_credentials = [
             'version' => 'latest',
             'region' => 'us-east-1',
-            'profile' => 'default',
-        ]);
+            'profile' => 'default'
+        ]; 
+        $this->f3 = $f3;
+    }  
+    
+    function home() {
+        print \Template::instance()->render('base.html');
+    }
+    
+    function login() {
+    	//Bring $f3 into scope
+        $f3 = $this->f3;
+        
+        //Set variables
+        $f3->set('current_time', self::get_current_time());
+        $f3->set('aws_ec2_instance', self::get_ec2_instance());
+		$f3->set('aws_rds_instance', self::get_rds_instance());
+		$f3->set('aws_cloudwatch_events', self::get_cloudwatch_events());
+		$f3->set('oauth_user', self::get_oauth_user()); 
+        
+		//Render Page
+        print \Template::instance()->render('base.html');
+    }
+    
+    function get_oauth_user() {
+    	$f3 = $this->f3;
+    	 
+    	// Create a new (and much lighter) OAuth2 client with no external dependencies!!  	   
+    	$oauth2 = new \Web\OAuth2();
+    	$auth_domain = 'https://stackoverflow.com';
+    	$redirect_uri = "{$f3->get('SCHEME')}://{$f3->get('HOST')}{$f3->get('BASE')}/login";
+    	
+    	//Set OAuth parameters
+    	$oauth2->set('redirect_uri', $redirect_uri);
+    	$oauth2->set('client_id', '12256');
+    	$oauth2->set('client_secret', '6J4fm2Ok73o20Q5dXO8ZhA((');
+
+    	if ($f3->get('VERB') == 'GET' && !isset($_GET['code'])) {
+    		//Step 1: OAuth 2.0 flow (Send a user to /oauth endpoint, with these query string parameters)
+    		header('Location: ' . $oauth2->uri($auth_domain . '/oauth'));
+    		// Step 2: OAuth 2.0 flow (The user approves your app)
+    	}
+    	elseif ($f3->get('VERB') == 'GET' && isset($_GET['code'])) {
+    		// Step 3: OAuth 2.0 flow (The user is redirected to redirect_uri, with these query string parameters)
+    		$oauth2->set('code', $_GET['code']);    	
+    		// Step 4: OAuth 2.0 flow (POST parameters to /oauth/access_token endpoint)
+    		$uri = $oauth2->uri($auth_domain . '/oauth/access_token/json');
+    		if ($request = $oauth2->request($uri, 'POST'))
+    		{
+    			//$oauth2 = new \Web\OAuth2();
+    			$web=\Web::instance();
+    			$oauth2->set('access_token', $request['access_token']);
+    			$oauth2->set('key', 'jYhy)W5d6ThTUK)iA60swg((');
+    			$oauth2->set('site', 'stackoverflow');
+    			$uri = $oauth2->uri('https://api.stackexchange.com/me',true,false);
+    			//$uri = "https://api.stackexchange.com/2.2/me?access_token={$request['access_token']}&key=jYhy)W5d6ThTUK)iA60swg((&site=stackoverflow";
+    			$options=[
+    					//'content'=>$url,
+    					'header'=>['Accept: application/json']
+    			];
+    			$me = $web->request($uri, $options);
+    			if ($me) {
+    				$f3->set('is_authenticated',true);
+    				return json_decode($me['body'],1)['items'][0];
+    			}
+    			$f3->set('is_authenticated',false);
+    		}
+    }
+    }
+    
+    //Get aws_ec2_instance
+    function get_ec2_instance () {    	
+    	//Get current instance ID
+    	$web=\Web::instance();
+    	$instance_id = $web->request('http://169.254.169.254/latest/meta-data/instance-id')['body'];
+
+    	//Create EC2 Client and perform query
+    	$client = new Aws\Ec2\Ec2Client($this->aws_credentials);
+    	$result = $client->describeInstances([
+    			'InstanceIds' => [$instance_id]
+    	]);
+
+    	//Set aws_ec2_instance properties
+		$output = [
+				'Name' => $result['Reservations'][0]['Instances'][0]['Tags'][0]['Value'],
+    			'Address' => $result['Reservations'][0]['Instances'][0]['PublicDnsName'],
+				'Identifier' => $instance_id
+    	];
+		$output['style'] = ($output['Name'] == 'pulser_web_1') ? 'badge-primary' : 'badge-success';
+    	return $output;
+    }
+
+    //Set current_time
+    function get_current_time () {return  date("g:i A l, F j Y.");}
+
+    //Get aws_rds_instance    
+    function get_rds_instance () {
+    	//Create RDS Client
+        $client = new Aws\Rds\RdsClient($this->aws_credentials);
         $result = $client->describeDBInstances();
-        $hostname = $result['DBInstances'][0]['Endpoint']['Address'];
-        $port = $result['DBInstances'][0]['Endpoint']['Port'];
-        $username = $result['DBInstances'][0]['MasterUsername'];
-        $dbconnection = mysqli_connect($hostname, $username, 'password');
-        
-        mysqli_close($dbconnection);
-        $f3->set('aws_rds_dbinstance_endpoint', $hostname);
-        $f3->set('aws_rds_dbinstance_id', $result['DBInstances'][0]['DBInstanceIdentifier']);
+
+        //Set aws_rds_instance properties
+        $output = [
+        	'Address' => $result['DBInstances'][0]['Endpoint']['Address'],
+        	'Port' => $result['DBInstances'][0]['Endpoint']['Port'],
+        	'MasterUsername' => $result['DBInstances'][0]['MasterUsername'],
+        	'Identifier' => $result['DBInstances'][0]['DBInstanceIdentifier'],
+        ];
+        $output['connection'] = mysqli_connect($output['Address'], $output['MasterUsername'], 'password'); 	
+		mysqli_close($output['connection']);
+        return $output;
         //$f3->set('DB', new DB\SQL("mysql:host={$hostname};port={$port};dbname={$database}",$username, $password));
-        
-        $client = new Aws\CloudWatchLogs\CloudWatchLogsClient([
-            'version' => 'latest',
-            'region' => 'us-east-1',
-            'profile' => 'default',
-        ]);
-        $result = $client->getLogEvents([
+    }
+
+    //Get aws_cloudwatch_events
+    function get_cloudwatch_events () {
+       $client = new Aws\CloudWatchLogs\CloudWatchLogsClient($this->aws_credentials);
+       $result = $client->getLogEvents([
 //            'endTime' => <integer>,
-//            'limit' => <integer>,
+            'limit' => 100,
             'logGroupName' => '/var/log/messages', // REQUIRED
             'logStreamName' => 'i-00b505e5c02ae863e', // REQUIRED
 //            'nextToken' => '<string>',
 //            'startFromHead' => true || false,
 //            'startTime' => <integer>,
-        ]); 
-        //$f3->set('debug', k($result['events'],KRUMO_RETURN)); 
-        $f3->set('aws_cloudwatch_events', $result['events']);
-        
-        $this->f3 = $f3;
-        
-    }  
-    
-    function home($f3) {
-        
-        print \Template::instance()->render('base.html');
-    }
-    
-    function login() {
-            // Create a new (and much lighter) OAuth2 client with no external dependencies!!
-        $f3 = $this->f3;
-        $oauth2 = new \Web\OAuth2();
-        $auth_domain = 'https://stackoverflow.com';
-        $redirect_uri = "{$f3->get('SCHEME')}://{$f3->get('HOST')}{$f3->get('BASE')}/login";
-        //$f3->set('debug', k($f3,KRUMO_RETURN));
-        //k($f3);
-        $oauth2->set('redirect_uri', $redirect_uri);
-        $oauth2->set('client_id', '12256');
-        //Step 1: OAuth 2.0 flow (Send a user to /oauth endpoint, with these query string parameters)
-        if ($f3->get('VERB') == 'GET' && !isset($_GET['code'])) {
-
-            // Set the endpoint's required params
-            //                 //$oauth2->set('state', $this->getRandomState());
-            $uri = $oauth2->uri($auth_domain . '/oauth');
-            
-            // Step 2: OAuth 2.0 flow (The user approves your app)
-            header('Location: ' . $uri);
-        }
-        // Step 3: OAuth 2.0 flow (The user is redirected to redirect_uri, with these query string parameters)
-        elseif ($f3->get('VERB') == 'GET' && isset($_GET['code'])) {
-                // Set the endpoint's required params
-            
-            
-                
-                //$oauth2->set('state', $_GET['state']);
-                $oauth2->set('client_secret', '6J4fm2Ok73o20Q5dXO8ZhA((');
-                $oauth2->set('code', $_GET['code']);
-                
-                // Step 4: OAuth 2.0 flow (POST parameters to /oauth/access_token endpoint)
-               $uri = $oauth2->uri($auth_domain . '/oauth/access_token/json');
-                if ($request = $oauth2->request($uri, 'POST'))
-                {
-                    $oauth2 = new \Web\OAuth2();
-                    $web=\Web::instance();
-
-                    $oauth2->set('access_token', $request['access_token']);
-                    $oauth2->set('key', 'jYhy)W5d6ThTUK)iA60swg((');
-                    $oauth2->set('site', 'stackoverflow');
-                    $uri = $oauth2->uri('https://api.stackexchange.com/me',true,false);
-                    //$uri = "https://api.stackexchange.com/2.2/me?access_token={$request['access_token']}&key=jYhy)W5d6ThTUK)iA60swg((&site=stackoverflow";
-                    $options=[
-                        'content'=>$url,
-                        'header'=>['Accept: application/json']
-                    ];
-                    $me = $web->request($uri, $options);
-                    if ($me) {
-                        $f3->set('is_authenticated',true);
-                        //$f3->set('me',k($me,KRUMO_RETURN));
-                        $f3->set('me',json_decode($me['body'],1)['items'][0]);
-                    }
-                        //k(json_decode($me['body']));
-                    //$f3->set('token', $request);
-                    
-                }
-                //$f3->set('debug', k($request, KRUMO_RETURN));
-                
-                    //$this->f3->set('token', $oauth2->jwt($request['access_token']));
-                    //F3::set('COOKIE.f3token', $request['access_token']);
-                 
-                
-        }
-        print \Template::instance()->render('base.html');
-        
+        ]);
+        return $result; 
     }
 }
-    
--- index.php
+++ index.php
@@ -10,26 +10,6 @@ $f3 = \Base::instance();
 
 //Load F3 Config
 $f3->config('config.ini');
-//$f3->set('debug', k($f3,KRUMO_RETURN));
-//Check to see if there is a database name already set in the F3 config or use the (cleaned) SERVER_NAME
-// if (!($f3->get('DB')))
-// {
-//     $site = new \Site($schema_name);
-//     if ($site->databaseReady() == true)
-//     {
-//         $driver = $site->get_db_driver();
-//         $host = $site->get_db_hostname();
-//         $port = $site->get_db_port();
-//         $username = $site->get_db_username();
-//         $password = $site->get_db_password();
-//         $db = new \DB\SQL("{$driver}:host={$host};port={$port};dbname={$schema_name}", $username, $password);
-//         $f3->set('DB', $db);
-//         //$site->set_db($db);
-//     }
-    
-//     if ($schema = $f3->get('schema')) {
-//         $site->schemaReady($schema);
-//     }
-    
-// }
+
+//Run F3
 $f3->run();
